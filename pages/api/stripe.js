2cec14cd51212a71586caa53b3ddef6cbc2deb48
Cesar Oliveira <cesarcristianodeoliveira@gmail.com>
2022-05-07 22:07:28 -0300

    brazilian currency

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
 
           return {
             price_data: { 
-              currency: 'usd',
+              currency: 'BRL',
               product_data: { 
                 name: item.name,
                 images: [newImage],
