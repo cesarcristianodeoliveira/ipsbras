f510095988d150d5b3293a2bef39cf938fb98a8d
Cesar Oliveira <cesarcristianodeoliveira@gmail.com>
2022-05-08 01:44:29 -0300

    Rage Against the Machine

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -10,13 +10,16 @@ export default async function handler(req, res) {
         mode: 'payment',
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
+        shipping_options: [
+          { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
+        ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
-          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/7yb25lzj/production/').replace('-webp', '.webp', '.png', '.jpg', '.jpeg');
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/7yb25lzj/production/').replace('-webp', '.webp');
 
           return {
             price_data: { 
-              currency: 'BRL',
+              currency: 'usd',
               product_data: { 
                 name: item.name,
                 images: [newImage],
