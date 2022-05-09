5019d95b5b722f1f0decf104629fe25b9e459a61
Cesar Oliveira <cesarcristianodeoliveira@gmail.com>
2022-05-09 10:56:28 -0300

    Configurando Stripe e Página do Produto

--- pages/produto/[slug].js
+++ pages/produto/[slug].js
@@ -8,6 +8,13 @@ const ProductDetails = ({ product, products }) => {
   const { image, name, price, content, highlights, details, review } = product
   const [index, setIndex] = useState(0);
   const { decQty, incQty, qty, onAdd } = useStateContext()
+
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+
+    setShowCart(true);
+  }
+
   return (
     <>
       <div>
@@ -81,7 +88,7 @@ const ProductDetails = ({ product, products }) => {
               Adicionar ao Carrinho
               </button>
               <button
-              type="submit"
+              onClick={handleBuyNow}
               className="mt-4 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
               Comprar Agora
