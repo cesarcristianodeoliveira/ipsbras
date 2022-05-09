52988eadeb1c2e41d01588ee14b02fbc326d67b1
Cesar Oliveira <cesarcristianodeoliveira@gmail.com>
2022-05-09 13:21:28 -0300

    Stripe

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -10,16 +10,13 @@ export default async function handler(req, res) {
         mode: 'payment',
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
-        shipping_options: [
-          { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
-        ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
           const newImage = img.replace('image-', 'https://cdn.sanity.io/images/7yb25lzj/production/').replace('-webp', '.webp', '.png', '.jpg', '.jpeg');
 
           return {
             price_data: { 
-              currency: 'BRL',
+              currency: 'br',
               product_data: { 
                 name: item.name,
                 images: [newImage],
