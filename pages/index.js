8cb277c624936bdc6fe77014b39a994df9b3914c
Cesar Oliveira <cesarcristianodeoliveira@gmail.com>
2022-05-11 15:27:03 -0300

    Versão 1.1

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Home = ({ products, banners }) => {
   return (
   <>
     <Banner heroBanner={banners.length && banners[0]}/>
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+    <div className="max-w-2xl mx-auto py-20 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
       <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 cursor-default">Produtos recentes</h2>
       <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {products?.map((product) => <Product key={product._id} product={product} />)}
