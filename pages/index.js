022c2b0384ccf0b4b5e8c7eb55c3932a9fca381b
Cesar Oliveira <cesarcristianodeoliveira@gmail.com>
2022-05-11 12:20:00 -0300

    Versão 1.1

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,9 @@ import { client } from '../lib/client';
 import { Banner, Product } from '../components';
 
 const Home = ({ products, banners }) => {
-  const randomBanner = banners[Math.floor(Math.random() * banners.length)]
   return (
   <>
-    <Banner heroBanner={randomBanner} />
+    <Banner heroBanner={banners.length && banners[0]}/>
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 cursor-default">Produtos recentes</h2>
       <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
@@ -19,7 +18,7 @@ const Home = ({ products, banners }) => {
 };
 
 export const getServerSideProps = async () => {
-  const queryProducts = '*[_type == "product"]';
+  const queryProducts = `*[_type == "product"]`;
   const products = await client.fetch(queryProducts);
 
   const queryBanners = `*[_type == "product" && isBanner == true] {
